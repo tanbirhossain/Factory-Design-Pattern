c910f341ceb37e68d5ab2a9d80fd488181420e95
Mohammed Tanbir Hossain <pasic.tanbir@hotmail.com>
2019-09-12 16:48:53 +0900

    update factory

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -8,14 +8,12 @@ namespace Factory
     {
         static void Main(string[] args)
         {
-
             //setup our DI
             var serviceProvider = new ServiceCollection()
                 .AddSingleton<IComputerFactory, ComputerFactory>()
                 .BuildServiceProvider();
 
             var factory = serviceProvider.GetService<IComputerFactory>(); // find our factory
-
 
 
             //call factory
@@ -29,6 +27,21 @@ namespace Factory
             Console.WriteLine(result_lenovo);
 
             Console.ReadLine();
+
         }
     }
 }
+// What kind of problem we are trying to solve using factory design pattern
+// what is factory design pattern
+// In the oop design its the most common and and useful desing patter. What actualy doing factory design patter. factory design its kind of object factory . in the real world you already heard the factory word. like food factory , machine factory etc. 
+// Factory is a collection of object . where you can deside which object you will get . this factory design pattern help us onject creation problem.
+
+//Lets focus of the problem. Suppose we have a 3 Computer Manufacturers . They providing support us . if customer face any problem we have to call the manufacturer based on their  
+//manufacturer brand name. so our main problem here create our manufaturer object based on theri brand name. 
+//Here we will create a factory . This factory help us create object.
+// Lets start coding
+//
+
+
+
+
