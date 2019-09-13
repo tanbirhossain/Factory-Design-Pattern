41545d6962905c2e064a3af56c35ecaaec1b0f07
Mohammed Tanbir Hossain <pasic.tanbir@hotmail.com>
2019-09-13 16:50:24 +0900

    introduce IOC

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -6,15 +6,12 @@ namespace Factory
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             //setup our DI
-            var serviceProvider = new ServiceCollection()
-                .AddSingleton<IComputerFactory, ComputerFactory>()
-                .BuildServiceProvider();
-
-            var factory = serviceProvider.GetService<IComputerFactory>(); // find our factory
-
+            var serviceProvider = ServiceProvider();
+            var factory = serviceProvider.GetService<IComputerFactory>(); // find our factory service
 
             //call factory
             var result_apple = factory.GetCompany("apple").DoWork();
@@ -29,18 +26,16 @@ namespace Factory
             Console.ReadLine();
 
         }
+
+        public static ServiceProvider ServiceProvider()
+        {
+            var provider = new ServiceCollection()
+             .AddSingleton<IComputerFactory, ComputerFactory>()
+             .BuildServiceProvider();
+            return provider;
+        }
     }
 }
-// What kind of problem we are trying to solve using factory design pattern
-// what is factory design pattern
-// In the oop design its the most common and and useful desing patter. What actualy doing factory design patter. factory design its kind of object factory . in the real world you already heard the factory word. like food factory , machine factory etc. 
-// Factory is a collection of object . where you can deside which object you will get . this factory design pattern help us onject creation problem.
-
-//Lets focus of the problem. Suppose we have a 3 Computer Manufacturers . They providing support us . if customer face any problem we have to call the manufacturer based on their  
-//manufacturer brand name. so our main problem here create our manufaturer object based on theri brand name. 
-//Here we will create a factory . This factory help us create object.
-// Lets start coding
-//
 
 
 
